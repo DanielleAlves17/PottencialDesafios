59fbb9c9360591e456a79484e75ff0233bfb5ddc
Danielle Alves Feih <89142483+DanielleAlves17@users.noreply.github.com>
2022-10-26 20:25:15 -0300

    Projeto da Api de Tarefas
    Projeto da Api de Tarefas

---
+++ TarefaApi/Context/OrganizadorContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TarefaApi.Models;
+
+
+
+namespace TarefaApi.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+        
+    
+    
+    
+}
